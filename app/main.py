2877bac8d9c4359d377e2c5f25b32b9b1063a27c
Marco Nunnari <hello@marconunnari.com>
2022-12-25 23:10:37 +0100

    Implement the SET & GET commands

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ import threading
 
 from .resp_decoder import RESPDecoder
 
+database = {}
+
 
 def handle_connection(client_connection):
     while True:
@@ -13,6 +15,15 @@ def handle_connection(client_connection):
                 client_connection.send(b"+PONG\r\n")
             elif command == b"echo":
                 client_connection.send(b"$%d\r\n%b\r\n" % (len(args[0]), args[0]))
+            elif command == b"set":
+                database[args[0]] = args[1]
+                client_connection.send(b"+OK\r\n")
+            elif command == b"get":
+                value = database.get(args[0])
+                if value is None:
+                    client_connection.send(b"+(nil)\r\n")
+                else:
+                    client_connection.send(b"$%d\r\n%b\r\n" % (len(value), value))
             else:
                 client_connection.send(b"-ERR unknown command\r\n")
         except ConnectionError:
